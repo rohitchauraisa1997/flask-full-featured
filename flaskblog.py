e232fe99e9b3dc5f85869f087dbeb3fcb431024a
Rohit Chaurasia <rohit.chaurasia997@gmail.com>
2021-04-10 18:53:58 +0900

    using templates to render data

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 # __name__ is a special variable in python
 # that is the name of the module this tells 
 # flask where to look for the tmplates, static files etc.
@@ -6,14 +6,29 @@ from flask import Flask
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'title':'One Piece',
+        'author': 'Eichiro Oda',
+        'content': 'one piece chapter 1',
+        'date_posted': 'Oct 1, 1997'
+    },
+    {
+        'title':'Naruto',
+        'author': 'Masashi Kishimoto',
+        'content': 'Naruto chapter 1',
+        'date_posted': 'Oct 1, 1997'
+    }
+]
+
 @app.route('/')
 @app.route('/home')
 def hello_world():
-    return '<h3>Hello, World!</h3>'
+    return render_template('home.html',posts=posts)
 
 @app.route('/about')
 def about():
-    return '<h3>Hello, About Page</h3>'
+    return render_template('about.html',title="About")
 
 if __name__ == '__main__':
     app.run(debug=True)
