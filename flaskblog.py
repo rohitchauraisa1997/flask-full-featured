eb9399aba69cf7b20c5dea5bcb36cae3902b384c
Rohit Chaurasia <rohit.chaurasia997@gmail.com>
2021-04-11 21:51:59 +0900

    adding User and Post model to our app

--- flaskblog.py
+++ flaskblog.py
@@ -1,15 +1,44 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
-# __name__ is a special variable in python
-# that is the name of the module this tells 
-# flask where to look for the tmplates, static files etc.
-# print(__name__)
 
 app = Flask(__name__)
-
-# secret key protects against modifying cookies,
-# crossite requests and forgery attacks.
 app.config['SECRET_KEY'] = "rohit97"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60),nullable=False)
+    # Meaning
+    # posts Attribute adds a relationship to the post model
+    # referring to Post Model Class
+    # backref is similar to having another col in the Posts Model to get the 
+    # author attribute to get the user who created the post.
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}','{self.email}','{self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    # remember not to use paranthesis in datetime.utcnow as it will return the
+    # default to be time at that period of time rather than passing the datetime.utcnow
+    # function.
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    # relationship to User model 
+    # user is table here hence the smallcasing of user in user.id
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    
+    def __repr__(self):
+        return f"Post('{self.title}','{self.date_posted}')"
 
 posts = [
     {
