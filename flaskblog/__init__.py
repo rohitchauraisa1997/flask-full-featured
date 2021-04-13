776e4e1648bde6451c55aeab4e7b6f8e2fa5d7b5
Rohit Chaurasia <rohit.chaurasia997@gmail.com>
2021-04-13 16:11:04 +0900

    pretty print the flashes

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -11,5 +11,7 @@ db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
+# just to fancy flash messages.
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
