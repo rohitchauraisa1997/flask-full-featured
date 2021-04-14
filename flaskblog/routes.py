69f61838ce989fca11bfd7c7f8dbf4831b2a00e5
Rohit Chaurasia <rohit.chaurasia997@gmail.com>
2021-04-15 00:48:34 +0900

    resizing to 125 pixels

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,5 +1,6 @@
 import secrets
 import os
+from PIL import Image
 from flask import  render_template, url_for, flash, redirect, request
 from flaskblog import app
 from flaskblog.models import User, Post
@@ -83,8 +84,14 @@ def save_picture(form_picture):
     file_name, file_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + file_ext
     picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    # resizing the picture before saving
+    # as only 125px gets rendered and also
+    # memory gets wasted. 
+    output_size = (125,125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
     # saves image to filesystem
-    form_picture.save(picture_path)
+    i.save(picture_path)
     return picture_fn
 
 @app.route("/account",methods=['GET', 'POST'])
